946ab8cb7879237f822c6adbba3cbbdebfc75c21
Karthik <Karthik@LAPTOP-HPI08USJ>
2017-10-18 10:39:26 +0500

    updated project

--- CourseAuthorApp/src/main/java/com/courseauthor/app/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.courseauthor.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.courseauthor.app.model.Author;
-
-public interface AuthorRepository extends JpaRepository<Author, Long>{
-
-}
