24f3343c99bcc20e1f31d260a5608497e07f50a1
Karthik <Karthik@LAPTOP-HPI08USJ>
2017-10-18 10:34:14 +0500

    updated

---
+++ CourseAuthorApp/src/main/java/com/courseauthor/app/model/Course.java
@@ -0,0 +1,45 @@
+package com.courseauthor.app.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Course implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String courseName;
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
